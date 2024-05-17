557af61139890d608358afd4fc084ca6ee639fc3
WonyeolHEO <hyy96hyy@naver.com>
2024-05-18 00:34:30 +0900

    AddView 컴포넌트 추가, 레이아웃 설정

--- Journal_Codeonly/AddJournalViewController.swift
+++ Journal_Codeonly/AddJournalViewController.swift
@@ -8,6 +8,56 @@
 import UIKit
 
 class AddJournalViewController: UIViewController {
+    
+    private lazy var mainContainer: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 40
+        return stackView
+    }()
+    
+    private lazy var ratingView: UIStackView = {
+        let stackView = UIStackView(frame: CGRect(x: 0, y: 0, width: 252, height: 44))
+        stackView.axis = .horizontal
+        stackView.backgroundColor = .systemCyan
+        return stackView
+    }()
+    
+    private lazy var toggleView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.alignment = .fill
+        stackView.distribution = .fill
+        stackView.spacing = 8
+        let switchComponent = UISwitch()
+        let labelComponent = UILabel()
+        labelComponent.text = "Switch Label"
+        
+        stackView.addArrangedSubview(switchComponent)
+        stackView.addArrangedSubview(labelComponent)
+        return stackView
+    }()
+    
+    private lazy var titleTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Enter journal Title"
+        return textField
+    }()
+    
+    private lazy var bodyTextView: UITextView = {
+        let textView = UITextView()
+        textView.text = "Journal Body"
+        return textView
+    }()
+    
+    private lazy var imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "face.smiling")
+        return imageView
+    }()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +71,41 @@ class AddJournalViewController: UIViewController {
         navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel,
                                                            target: self,
                                                            action: #selector(cancel))
+        
+        mainContainer.addArrangedSubview(ratingView)
+        mainContainer.addArrangedSubview(toggleView)
+        mainContainer.addArrangedSubview(titleTextField)
+        mainContainer.addArrangedSubview(bodyTextView)
+        mainContainer.addArrangedSubview(imageView)
+        
+        view.addSubview(mainContainer)
+        mainContainer.translatesAutoresizingMaskIntoConstraints = false
+        ratingView.translatesAutoresizingMaskIntoConstraints = false
+        toggleView.translatesAutoresizingMaskIntoConstraints = false
+        titleTextField.translatesAutoresizingMaskIntoConstraints = false
+        bodyTextView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            mainContainer.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 20),
+            
+            mainContainer.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            mainContainer.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
+            
+            ratingView.widthAnchor.constraint(equalToConstant: 252),
+            ratingView.heightAnchor.constraint(equalToConstant: 44),
+            
+            titleTextField.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            titleTextField.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            
+            bodyTextView.leadingAnchor.constraint(equalTo: mainContainer.leadingAnchor, constant: 8),
+            bodyTextView.trailingAnchor.constraint(equalTo: mainContainer.trailingAnchor, constant: -8),
+            bodyTextView.heightAnchor.constraint(equalToConstant: 128),
+            
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
         
         
         
