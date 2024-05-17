0c7e5c2af4e22681773f57af1d53562728b4b094
WonyeolHEO <hyy96hyy@naver.com>
2024-05-17 20:46:23 +0900

    github setting - Create Project and gitignore

---
+++ Journal_Codeonly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Journal_Codeonly
+//
+//  Created by wonyoul heo on 5/17/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
