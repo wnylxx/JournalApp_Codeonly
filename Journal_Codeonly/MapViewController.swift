302d6d935ff09eabc368080a1043fab15a678795
WonyeolHEO <hyy96hyy@naver.com>
2024-05-19 14:54:40 +0900

    MapViweController 위치정보 업데이트

--- Journal_Codeonly/MapViewController.swift
+++ Journal_Codeonly/MapViewController.swift
@@ -7,8 +7,12 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
+    
     private lazy var mapView: MKMapView = {
         mapView = MKMapView()
         mapView.translatesAutoresizingMaskIntoConstraints = false
@@ -19,7 +23,12 @@ class MapViewController: UIViewController {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-        navigationItem.title = "Map"
+        
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        self.navigationItem.title = "Loading..."
+        locationManager.requestLocation()
+        
         view.addSubview(mapView)
         
         let safeArea = view.safeAreaLayoutGuide
@@ -31,4 +40,23 @@ class MapViewController: UIViewController {
         ])
     }
     
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let myCurrentLocation = locations.first {
+            let lat = myCurrentLocation.coordinate.latitude
+            let long = myCurrentLocation.coordinate.longitude
+            
+            self.navigationItem.title = "Map"
+            mapView.region = setInitialRegion(lat: lat, long: long)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: any Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+    
+    
+    func setInitialRegion(lat: CLLocationDegrees, long: CLLocationDegrees) -> MKCoordinateRegion {
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: lat, longitude: long),
+                           span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    }
 }
